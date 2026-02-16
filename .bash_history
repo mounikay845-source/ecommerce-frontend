66846a394a706bacfd76a70e2b5c728f62d7e94f
Mounika Yadav <mounikay845@gmail.com>
2026-02-16 12:30:25 +0530

    fixed hamburger menu

---
+++ .bash_history
@@ -0,0 +1,8 @@
+cd 'git config'/
+cd desktop
+cd 'git config'/
+cd program files
+cd git bash
+cd 'git bash'
+cd 'git bash'
+cd 'git-bash'
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
 console.log("E-Commerce Website Loaded");
+
+// Select elements
+const hamburger = document.querySelector(".hamburger");
+const menu = document.getElementById("menu");
+
+// Toggle menu
+hamburger.addEventListener("click", function () {
+    menu.classList.toggle("active");
+});
